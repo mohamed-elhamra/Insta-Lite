b5748cf952c8126bf767e17836908aa1419dbc86
hmoubarik <hanae.moubarik@soprasteria.com>
2023-02-01 17:23:22 +0100

    (client) : La création du profil
    Dans cette branche on a créé le fichier d'environnement, la création du
    service pour (la connexion et l'ajout d'un utilisateur) ainsi le composant de
    la création du compte

---
+++ client/src/app/_services/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+const AUTH_API = environment.apiUrl + "/users/";
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(AUTH_API + 'authenticate',{
+      email,
+      password
+    }, httpOptions ); 
+  }
+
+  register(firstname: string, lastname: string, email: string, password:string, role: Number): Observable<any> {
+    return this.http.post(AUTH_API, {
+      firstname,
+      lastname,
+      email,
+      password,
+      role
+    }, httpOptions);
+  }
+}
