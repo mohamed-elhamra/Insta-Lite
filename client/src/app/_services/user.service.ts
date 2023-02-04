5dd6faec0b5f70ab06479415c00e1078d46059e7
hanae.moubarika@gmail.com <hanae.moubarika@gmail.com>
2023-02-04 02:19:45 +0100

    feat (client) : manage users and authorizations

--- client/src/app/_services/user.service.ts
+++ client/src/app/_services/user.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { TokenStorageService } from './token-storage.service';
+
+
+const AUTH_API = environment.apiUrl + "/users/";
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private tokenStorage: TokenStorageService) { }
+
+  getUsersList(): Observable<any> {
+    return this.http.get(`${AUTH_API}`);
+  }
+
+  deleteUser(publicId: string): Observable<any> {
+    return this.http.delete(`${AUTH_API}${publicId}`, { responseType: 'text' });
+  }
+
+  updateUser(publicId: string, value: any): Observable<Object> {
+    return this.http.patch(`${AUTH_API}${publicId}`, JSON.stringify(value, this.tokenStorage.getUser()));
+  }
+
+  getUser(publicId: string): Observable<any> {
+    return this.http.get(`${AUTH_API}${publicId}`);
+  }
 }
